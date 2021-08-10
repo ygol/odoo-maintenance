2180b06bc8ebe0bb45cd83b2b73bc1f839cae1ba
Anders Wallenquist <anders.wallenquist@vertel.se>
2021-08-10 13:13:01 +0000

    maintenance_equipment_request_copy: new module

--- maintenance_equipment_request_copy/__manifest__.py
+++ maintenance_equipment_request_copy/__manifest__.py
@@ -1,16 +1,16 @@
 
 {
-    "name": "Maintenance Equipment Operation",
+    "name": "Maintenance Equipment Request Copy",
     "version": "14.0.1.0.0",
     "author": "Vertel AB",
     "license": "AGPL-3",
     "category": "other",
-    "summary": "Adds operation on equipment",
-    "website": "https://vertel.se/apps/maintenance_equipment_operation",
+    "summary": "Wizard for copying request on one or more equipments",
+    "website": "https://vertel.se/apps/maintenance_equipment_request_copy",
     "depends": ["maintenance"],
     "data": [
-        "views/maintenance_views.xml",
         "security/ir.model.access.csv",
+        "wizard/maintenance_views.xml",
         ],
     "installable": True,
     "application": False,
