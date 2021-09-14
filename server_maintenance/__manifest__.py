d4f7998d7c6407167a810cb9f148c331616f5b70
Miracle Ayodele <miracleaayodele@gmail.com>
2021-09-14 13:36:08 +0100

    update server status

---
+++ server_maintenance/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+{
+    'name': 'Server Monitoring',
+    'version': '14.0.1.0.0',
+    'author': 'Vertelab',
+    'license': 'AGPL-3',
+    'category': 'category',
+    'depends': ['maintenance', 'monitoring_status'],
+    'website': 'https://www.vertelab.com',
+    'data': [
+        'views/maintenance_view.xml'
+    ],
+    'installable': True,
+}
---
+++ server_maintenance/controllers/main.py
@@ -0,0 +1,19 @@
+import logging
+import json
+from odoo import models, api, _, http
+
+import werkzeug
+import os
+from odoo.addons.monitoring_status.controllers.main import Monitoring
+
+
+class ServerMonitoring(Monitoring):
+
+    def get_status(self):
+        info = {'status': 1}
+        info.update({
+            'space_availability': os.popen('df -h').read(),
+            'load': os.popen('cat /proc/loadavg ; top').read()
+        })
+        return info
+
