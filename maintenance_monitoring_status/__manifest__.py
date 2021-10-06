8ff993833d5e0fdf96add5ea69d5c9ec86ccf54b
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2021-10-06 07:27:18 +0000

    maintenance_equipment_monitoring,maintenance_monitoring_status,maintenance_monitoring_status_extension: I have added larger descriptions.

--- maintenance_monitoring_status/__manifest__.py
+++ maintenance_monitoring_status/__manifest__.py
@@ -10,8 +10,13 @@
     'category': 'category',
     'description': """
        This module is maintained from: https://github.com/vertelab/odoo-maintenance/ \n 
-       until https://github.com/camptocamp/odoo-cloud-platform/tree/14.0/monitoring_status has lifted it.
-       Inspired by Camptocamp SA.
+       until https://github.com/camptocamp/odoo-cloud-platform/tree/14.0/monitoring_status has lifted it.\n
+       Inspired by Camptocamp SA.\n
+       
+       Purpose of this module is to answer with a json object the status of the database, on or off. To query the database you need to go to the /monitoring/status url.
+       Url Syntax: http ://hostname/monitoring/status?db=databasename\n
+       
+       In order for the database to be able to answer without being logged in on you need to change the odoo.conf and add this module to the server_wide_modules.
     """,
     'depends': ['base', 'web'],
     'website': 'https://www.camptocamp.com',
