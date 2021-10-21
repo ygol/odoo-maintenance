a84f631d33d252ea573fdb5a469eb02772ba4ab5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 14:41:44 +0200

    Update __manifest__.py

--- maintenance_monitoring_status_extension/__manifest__.py
+++ maintenance_monitoring_status_extension/__manifest__.py
@@ -1,13 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 # Copyright 2016-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-
 {
     'name': 'Server Monitoring',
+    'summary': 'To be able to have.....',
+    'author': 'Vertel AB',
+    'contributor': 'Mitchell Admin',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-maintenance.git',
+    'category': 'Tools',
     'version': '14.0.1.0.0',
-    'author': 'Vertelab',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'category': 'category',
+    'website': 'https://vertel.se',
     'description': """
        This module is maintained from: https://github.com/vertelab/odoo-maintenance/
 
@@ -18,7 +44,6 @@
        In order for the database to be able to answer without being logged in on you need to change the odoo.conf and add this "maintenance_montitoring_status" to the server_wide_modules.
     """,
     'depends': ['maintenance_monitoring_status'],
-    'website': 'https://www.vertelab.com',
     'data': [
     ],
     'installable': True,
