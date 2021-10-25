713e4ca60b1762362ab61f27ae2869fa47bf0871
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-25 15:53:59 +0200

    Update __manifest__.py

--- maintenance_equipment_operation/__manifest__.py
+++ maintenance_equipment_operation/__manifest__.py
@@ -1,12 +1,38 @@
 
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    "name": "Maintenance Equipment Operation",
-    "version": "14.0.1.0.0",
-    "author": "Vertel AB",
-    "license": "AGPL-3",
-    "category": "other",
-    "summary": "Adds operation on equipment",
-    "website": "https://vertel.se/apps/maintenance_equipment_operation",
+    'name': 'Maintenance Equipment Operation',
+    'summary': 'Adds operation on equipment.',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-maintenance',
+    'category': 'Tools',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/apps/maintenance',
+    # "website": "https://vertel.se/apps/maintenance_equipment_operation",
     "depends": ["maintenance"],
     "data": [
         "views/maintenance_views.xml",
