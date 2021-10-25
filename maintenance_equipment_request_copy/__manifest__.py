02e1605b1c79962af1e5ac5ced73d9e9726ab1d3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-25 16:04:12 +0200

    Update __manifest__.py

--- maintenance_equipment_request_copy/__manifest__.py
+++ maintenance_equipment_request_copy/__manifest__.py
@@ -1,12 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-    "name": "Maintenance Equipment Request Copy",
-    "version": "14.0.1.0.0",
-    "author": "Vertel AB",
-    "license": "AGPL-3",
-    "category": "other",
-    "summary": "Wizard for copying request on one or more equipments",
-    "website": "https://vertel.se/apps/maintenance_equipment_request_copy",
+    'name': 'Maintenance Equipment Request Copy',
+    'summary': 'Wizard for copying request on one or more equipments',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-maintenance',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/apps/maintenance',
+    #"website": "https://vertel.se/apps/maintenance_equipment_request_copy",
+    'description': '',
     "depends": ["maintenance"],
     "data": [
         "security/ir.model.access.csv",
