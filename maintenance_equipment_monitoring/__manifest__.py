02e099a51aed03d971b338a727c22c94bed5bde9
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-10-29 12:22:18 +0000

    maintenance_equipment_monitoring: pep8 formating

--- maintenance_equipment_monitoring/__manifest__.py
+++ maintenance_equipment_monitoring/__manifest__.py
@@ -23,30 +23,34 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Maintenance equipment monitoring',
-    'summary': 'For other Odoo databases to monitor the status of their server.',
-    'author': 'Vertel AB',
-    'contributor': '',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-maintenance',
-    'version': '14.0.1.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/maintenance',
-    'description': """
-       The purpose of this module is to monitor other odoo databases to see the status of their server. In order to query other databases the need
+    "name": "Maintenance equipment monitoring",
+    "summary": "For other Odoo databases to monitor the status of their server.",
+    "author": "Vertel AB",
+    "contributor": "",
+    "maintainer": "Vertel AB",
+    "repository": "https://github.com/vertelab/odoo-maintenance",
+    "version": "14.0.1.0.1",
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code.
+    # 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    "license": "AGPL-3",
+    "website": "https://vertel.se/apps/maintenance",
+    "description": """
+       The purpose of this module is to monitor other odoo databases to see the
+       status of their server. In order to query other databases the need
        "maintenance_monitoring_status" or "maintenance_monitoring_status_extension" installed.
 
-       In order to specify which database you want to monitor you fill the  Monitor URL field on the equipment view and there is  a ping button to trigger this query.
+       In order to specify which database you want to monitor you fill the Monitor URL
+       field on the equipment view and there is  a ping button to trigger this query.
        The url needs to look like: http ://hostname/monitoring/status?db=databasename.
 
-       There is also cron job called "Ping Remote Server" that will ping every 60 minutes to all equipment that has "Is Monitored" field set to true.
+       There is also cron job called "Ping Remote Server" that will ping every
+       60 minutes to all equipment that has "Is Monitored" field set to true.
     """,
-    'depends': ['maintenance', 'maintenance_monitoring_status_extension'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/maintenance_view.xml',
-        'data/cron.xml'
+    "depends": ["maintenance", "maintenance_monitoring_status_extension"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/maintenance_view.xml",
+        "data/cron.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
