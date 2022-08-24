17cd02ef982c79493d311a2b474a3d081f43676a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-24 14:35:48 +0200

    Update __manifest__.py

--- maintenance_equipment_request_copy/__manifest__.py
+++ maintenance_equipment_request_copy/__manifest__.py
@@ -29,8 +29,7 @@
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/maintenance',
-    #"website": "https://vertel.se/apps/maintenance_equipment_request_copy",
+    'website': 'https://vertel.se/apps/odoo-maintenance/maintenance_equipment_request_copy',
     'description': '',
     "depends": ["maintenance"],
     "data": [
