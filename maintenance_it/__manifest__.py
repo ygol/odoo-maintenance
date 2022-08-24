35a53f5f7bc8f45d311a6957cc035b701ea68987
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-24 14:36:23 +0200

    Update __manifest__.py

--- maintenance_it/__manifest__.py
+++ maintenance_it/__manifest__.py
@@ -29,7 +29,7 @@
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/maintenance',
+    'website': 'https://vertel.se/apps/odoo-maintenance/maintenance_it',
     'description': """
         Adding helper IT fields such as IP, MAC and domain.
 
