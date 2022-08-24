6e6107c29b66d93ab1640182461041f28cc0507c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-08-24 14:33:38 +0200

    Update __manifest__.py

--- maintenance_cve/__manifest__.py
+++ maintenance_cve/__manifest__.py
@@ -29,7 +29,7 @@
     "version": "14.0.0.1.0",
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     "license": "AGPL-3",
-    "website": "https://vertel.se/apps/maintenance",
+    "website": "https://vertel.se/apps/odoo-maintenance/maintenance_cve",
     "description": """Generic tool to help an organization to keep track of CVEs and the work behind.""",
     "depends": ["maintenance", "website", "portal"],
     "data": [
